57d3f1ba167280533e73e42645166a20b1477e93
MichValenz <michvalenz27@gmail.com>
2021-12-12 14:15:13 -0600

    got API link to work for getWeather function

--- Assets/scripts.js
+++ Assets/scripts.js
@@ -1,64 +1,58 @@
-let apiKey = "7fee86a490b6cb5d1e5a78e521dbeb22";
-let searchTextEL = document.getElementById('textInput')
-let searchDiv = document.querySelector('control')
+//let apiKey = "7fee86a490b6cb5d1e5a78e521dbeb22";
+let searchText = document.getElementById('textInput')
+let searchDiv = document.querySelector('#btn')
 
-console.log("see this")
 
 function searchCity(){
 
-    searchDiv.addEventListener('submit', function(event){
+    searchDiv.addEventListener('click', function(event){
       // prevent page from refreshing
-     
+
       event.preventDefault();
 
     // get value from input element
-     let searchedCity = searchTextEL.value;
+     let searchedCity = searchText.value;
    
-     console.log(searchedCity)
+   
 
         if (isNaN(searchedCity)) {
 
         getWeather(searchedCity);
-      
+      console.log(searchedCity);
     }
     
    })
 
 }
+searchCity();
 
-let getWeather = function(searchedCity) {
-    let apiURL = (
-      "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity +
-      "&units=imperial" +
-      "&APPID=" +
-      apiKey
-    )
+// 2. function to fetch API
+
+function getWeather(searchedCity) {
+    let apiURL =
+      `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&units=imperial&appid=7fee86a490b6cb5d1e5a78e521dbeb22`;
 
       // make a get request to url
-      fetch(apiURL)
-      .then(function(response){
+      fetch(apiURL).then(function(response){
+        
+        if (response.ok) {
+        console.log(response);
+        response.json().then(function(data) {
 
-        if (response.ok){
-            console.log(response);
-            response.json().then(function(data) {
-                console.log(data);
-                displayWeather(data, city);
-            });
+     
 
+          // console.log(temp.main[0].id.temp);
+            //displayWeather(data, city);
+         });
+         } 
         }
-        else {
-                alert("Error: " + response.statusText);
-            }
-        });
-    };
+      );
+  }
 
-
-
-// 1. function to fetch API
 
 
 //2. when search is clicked it pulls weather api
 
 
 //3. function to display weather information which includes making current current templates update
-searchCity();
+
